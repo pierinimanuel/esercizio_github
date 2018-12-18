41c82bf3737d01d4b4a3cbb95e7ed8827429686c
manuel.melchiorri7846 <manuel.melchiorri7846@hotmail.com>
2018-12-18 15:33:52 +0100

    ho fatto la somma di due numeri

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("ciao");
+
+			int num1 = 0;
+			int num2 = 0;
+			int somma = 0;
+			Console.WriteLine("inserisci il primo numero");
+			num1 = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("inserisci il secondo numero");
+			num2 = Convert.ToInt32(Console.ReadLine());
+
+            somma = num1 + num2;
+
+			Console.WriteLine("La somma è{0}", somma);
+
+
+			Console.ReadLine();
+
+
+		}
+	}
+}
